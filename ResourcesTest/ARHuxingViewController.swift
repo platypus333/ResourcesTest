71ce1f32fcca7be0b25119d555039cb3dcc0fb36
platypus333 <qtinghao@163.com>
2018-12-19 17:46:38 +0800

    更新

--- ResourcesTest/ARHuxingViewController.swift
+++ ResourcesTest/ARHuxingViewController.swift
@@ -8,22 +8,64 @@
 
 import UIKit
 
-class ARHuxingViewController: UIViewController {
-
+public class ARHuxingViewController: UIViewController {
+    
     public var test:String = "test"
     
-    override func viewDidLoad() {
+    override public func viewDidLoad() {
         super.viewDidLoad()
         
         let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
-        imageView.image = getImage("add_icon")
+        imageView.image = getImage(imageName: "add_icon@2x", bundleName: "ResourcesTest", podName: "ResourcesTest")
         self.view.addSubview(imageView)
     }
-
-    private func getImage(_ name:String) -> UIImage{
-        let currentBundle = Bundle(path: Bundle(for: ARHuxingViewController.self).path(forResource: "ResourcesTest", ofType: "bundle")!)
-        return UIImage(contentsOfFile: (currentBundle?.path(forResource: name, ofType: "png"))!)!
+    
+    func getBundle(bundleName:String, podName:String) -> Bundle?
+    {
+        var bundleName = bundleName
+        var podName = podName
+        if bundleName == "" && podName == ""
+        {
+            return nil
+        }
+        else if bundleName == ""
+        {
+            bundleName = podName
+        }
+        else if podName == ""
+        {
+            podName = bundleName
+        }
+        
+        if bundleName.contains(".bundle")
+        {
+            bundleName = bundleName.components(separatedBy: ".bundle").first!
+        }
+        
+        var associateBundleURL = Bundle.main.url(forResource: bundleName, withExtension: "bundle")
+        if associateBundleURL == nil
+        {
+            associateBundleURL = Bundle.main.url(forResource: "Frameworks", withExtension: nil)
+            associateBundleURL = associateBundleURL?.appendingPathComponent(podName)
+            associateBundleURL = associateBundleURL?.appendingPathExtension("framework")
+            let associateBunle = Bundle(url: associateBundleURL!)
+            associateBundleURL = associateBunle?.url(forResource: bundleName, withExtension: "bundle")
+            return nil
+        }
+        else
+        {
+            return Bundle(url: associateBundleURL!)!
+        }
     }
-
+    
+    func getImage(imageName:String,bundleName:String, podName:String) -> UIImage?
+    {
+        let bundle = getBundle(bundleName:bundleName, podName:podName)
+        if bundle != nil
+        {
+            return UIImage(contentsOfFile: (bundle?.path(forResource: imageName, ofType: "png"))!)
+        }
+        return nil
+    }
 }
 
