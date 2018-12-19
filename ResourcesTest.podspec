51f07aaa802aab9dd13d5b6e93f695146518dd73
platypus333 <qtinghao@163.com>
2018-12-19 19:48:00 +0800

    更新

--- ResourcesTest.podspec
+++ ResourcesTest.podspec
@@ -1,7 +1,7 @@
 Pod::Spec.new do |s|
 
   s.name         = "ResourcesTest"
-  s.version      = "0.0.4"
+  s.version      = "0.0.5"
   s.summary      = "ResourcesTestSummary"
   s.description  = <<-DESC
 	ResourcesTestDescription
--- ResourcesTest/ARHuxingViewController.swift
+++ ResourcesTest/ARHuxingViewController.swift
@@ -16,9 +16,20 @@ public class ARHuxingViewController: UIViewController {
         super.viewDidLoad()
         
         let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
-        imageView.image = UIImage(named: "add_icon@2x")
+        imageView.image = arrowImage
         self.view.addSubview(imageView)
     }
     
+    var ResourcesTestBundle: Bundle{
+        return Bundle.init(path:Bundle.init(for: ARHuxingViewController.self).path(forResource: "ResourcesTest", ofType: "bundle")!)!
+    }
+    
+    var arrowImage: UIImage{
+        let path = Bundle.main.path(forResource: "ResourcesTest", ofType: "bundle")
+        let bundle = Bundle(path: path!)
+        let file = bundle?.path(forResource: "add_icon@2x", ofType: "png")
+        return UIImage(contentsOfFile: file!)!
+//        return (UIImage.init(contentsOfFile: self.ResourcesTestBundle.path(forResource: "add_icon@2x", ofType: "png")!)?.withRenderingMode(UIImage.RenderingMode.alwaysTemplate))!
+    }
 }
 
