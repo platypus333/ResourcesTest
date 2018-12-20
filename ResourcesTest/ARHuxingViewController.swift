193b3a6a7fe520d06336151031c5b8cc11472e38
platypus333 <qtinghao@163.com>
2018-12-20 10:38:06 +0800

    更新

--- ResourcesTest/ARHuxingViewController.swift
+++ ResourcesTest/ARHuxingViewController.swift
@@ -11,35 +11,36 @@ import UIKit
 public class ARHuxingViewController: UIViewController {
     
     public var test:String = "test"
-    
+    public var completeBack:((String) -> (Void))?
+
     override public func viewDidLoad() {
         super.viewDidLoad()
         
         let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
         imageView.image = arrowImage
         self.view.addSubview(imageView)
+        let tap = UITapGestureRecognizer(target: self, action: #selector(backAction))
+        imageView.isUserInteractionEnabled = true
+        imageView.addGestureRecognizer(tap)
     }
     
-    var ResourcesTestBundle: Bundle{
-        return Bundle.init(path:Bundle.init(for: ARHuxingViewController.self).path(forResource: "ResourcesTest", ofType: "bundle")!)!
-    }
-    
-    var arrowImage: UIImage{
-    
+    var resourcesTestBundle: Bundle{
         let frameworkBundle = Bundle(for: ARHuxingViewController.self)
         let bundleUrl = frameworkBundle.resourceURL?.appendingPathComponent("ResourcesTest.bundle")
         let resourceBundle = Bundle(url: bundleUrl!)
-        let image = UIImage(named: "add_icon@2x", in: resourceBundle, compatibleWith: nil)
-        return image!
-        
-//        let path = Bundle.main.path(forResource: "ResourcesTest", ofType: "bundle")
-//        let bundle = Bundle(path: path!)
-//        let file = bundle?.path(forResource: "add_icon@2x", ofType: "png")
-//        return UIImage(contentsOfFile: file!)!
-//        return (UIImage.init(contentsOfFile: self.ResourcesTestBundle.path(forResource: "add_icon@2x", ofType: "png")!)?.withRenderingMode(UIImage.RenderingMode.alwaysTemplate))!
+        return resourceBundle!
     }
     
+    var arrowImage: UIImage{
+        let image = UIImage(named: "add_icon@2x", in: resourcesTestBundle, compatibleWith: nil)
+        return image!
+    }
     
+    @objc func backAction()
+    {
+        completeBack!("1111")
+        self.dismiss(animated: true, completion: nil)
+    }
     
 }
 
